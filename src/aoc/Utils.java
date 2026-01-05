1fcb08b423e914b7b1a047c8e09f7f81e1befdbf
RubenSaunders <ruben.saunders@fx.sgx.com>
2026-01-05 17:42:01 +0000

    Utility functions

--- src/aoc/Utils.java
+++ src/aoc/Utils.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -15,6 +16,9 @@ public class Utils
         return Paths.get(".").toAbsolutePath().normalize().toString();
     }
 
+    /**
+     * Get the directory that the challenge files for challenge day/year are situated in
+     */
     public static String getDirectory(int year, int day)
     {
         return currentWorkingDirectory() + File.separator +
@@ -24,6 +28,11 @@ public class Utils
             "day" + day + File.separator;
     }
 
+    /**
+     * Read lines from a file, return list of them mapped using some function (excluding empty strings and null return values)
+     * @param filename File to read
+     * @param mapper Function to process each line
+     */
     public static <T> List<T> readLines(String filename, Function<String, T> mapper)
     {
         try (FileReader reader = new FileReader(filename))
@@ -32,12 +41,27 @@ public class Utils
                 .map(String::trim)
                 .filter(Predicate.not(String::isEmpty))
                 .map(mapper)
+                .filter(Objects::nonNull)
                 .toList();
         }
         catch (IOException e)
         {
-            System.err.printf("Failed to read file <%s> - %s%n", filename, e);
-            return List.of();
+            throw new RuntimeException(String.format("Failed to read file <%s> - %s%n", filename, e));
+        }
+    }
+
+    /**
+     * Return the text contents (trimmed) of a file
+     */
+    public static String readFile(String filename)
+    {
+        try (FileReader reader = new FileReader(filename))
+        {
+            return reader.readAllAsString().trim();
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(String.format("Failed to read file <%s> - %s%n", filename, e));
         }
     }
 }
