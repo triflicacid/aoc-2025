c4b4799012d026e488d22e606c2a6020f85a3abf
RubenSaunders <ruben.saunders@fx.sgx.com>
2026-01-06 15:58:04 +0000

    Added challenge template

---
+++ template/Main.java
@@ -0,0 +1,35 @@
+package aoc.y2025.day1;
+
+import aoc.Challenge;
+import aoc.Utils;
+
+import java.util.List;
+
+/**
+ * PART 1
+ * - Mock data: -
+ * - Question: -
+ * PART 2:
+ * - Mock data: -
+ * - Question: -
+ */
+public class Main
+{
+    public static final Challenge CHALLENGE = new Challenge(2025, 0);
+
+    private static final boolean DEBUG = false;
+    private static final String INPUT_FILE = "mock_data.txt";
+    private static final int PART = 1;
+
+    static void main(String[] args)
+    {
+        Utils.hello(CHALLENGE);
+        run();
+    }
+
+    private static void run()
+    {
+        String filename = CHALLENGE.path() + INPUT_FILE;
+        // todo
+    }
+}
